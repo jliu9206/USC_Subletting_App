137ff8c70fa0e0262d45bea22468e7d38486d805
bjchung8 <bjchung@usc.edu>
2024-04-15 22:34:07 -0700

    comments User.java

--- Java Resources/User.java
+++ Java Resources/User.java
@@ -21,20 +21,12 @@ class User {
         return userID;
     }
 
-    public void setUserID(Integer userID) {
-        this.userID = userID;
-    }
-
     public String getUsername() {
         return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public void setPassword(String password) {
