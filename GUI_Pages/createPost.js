2969d66f833d3d63589116836798cd12078c8c47
Eric Chen <ericsc@usc.edu>
2024-04-29 17:23:00 -0700

    Move JS back to createPost.js out of HTML file, get renterID with JDBC from username, revert data titles for GSON parsing correctness

--- GUI_Pages/createPost.js
+++ GUI_Pages/createPost.js
@@ -1,8 +1,43 @@
 document.getElementById("submit-button").addEventListener('click', function() {
-    const renter = localStorage.getItem("ID"); //make sure this is the renter ID from the renter table.
-    event.preventDefault();
+	event.preventDefault();
+
+	const username = localStorage.getItem("username");
+	let renter = null;
+	
+	const usernameJson = {
+		Username: username
+	}
+	fetch('ValidateRenterServlet', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(usernameJson)
+    })
+    .then(response => {
+        if (response.ok)
+        {
+            alert("Successfully sent username");
+        }
+        else 
+        {
+            return response.json();
+        }
+	})
+	.then(renterId => {
+		if(renterId["renterId"] == null || renterId["renterId"] == 0){
+			alert("renterId null");
+		}
+		else{
+			console.log(renterId["renterId"]);
+			renter = renterId["renterId"];
+		}
+	})
+	.catch(error => {
+		alert(error);
+	}); 
+	
     const listingTitle = document.getElementById("listingTitle").value;
-    // let propertyType = document.getElementById("propertyType").value;
     const propertyType = document.getElementById("propertyType").value;
     const address = document.getElementById("address").value;
     const monthlyPrice = document.getElementById("monthlyPrice").value;
@@ -13,7 +48,7 @@ document.getElementById("submit-button").addEventListener('click', function() {
     const size = document.getElementById("size").value;
     const description = document.getElementById("description").value;
     //check that all fields are filled
-    if (listingTitle.length === 0 || propertyType.length === 0 || propertyPrice.length === 0 || address.length == 0
+    if (listingTitle.length === 0 || propertyType.length === 0 || monthlyPrice.length === 0 || address.length == 0
         || monthlyPrice.length === 0 || bedrooms.length === 0 || bathrooms.length === 0 || startDate.length === 0
         || endDate.length === 0 || size.length === 0 || description.length === 0)
     {
