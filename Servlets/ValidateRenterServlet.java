83b7943d331bbbe634584acb1ca04e6abe76bcac
Eric Chen <ericsc@usc.edu>
2024-04-29 17:24:09 -0700

    servlet for getting renterID in createPost

---
+++ Servlets/ValidateRenterServlet.java
@@ -0,0 +1,47 @@
+package finalproject;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+@WebServlet("/ValidateRenterServlet")
+public class ValidateRenterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public ValidateRenterServlet() {
+        super();
+    }
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = new PrintWriter(response.getWriter());
+		Connection conn = JDBCPost.connectSQL("root", "[your password]");
+		
+		Gson g = new Gson();
+		int renterId = JDBCPost.getRenterId(conn, request.getParameter("username"));
+		RenterId id = new RenterId(renterId);
+		String jsonString = g.toJson(id);
+		
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		System.out.println("got the renterid");
+		pw.write(jsonString);
+		pw.flush();
+		pw.close();
+	}
+
+}
+
+class RenterId{
+	public int renterId;
+	public RenterId(int id) {
+		renterId = id;
+	}
+}
